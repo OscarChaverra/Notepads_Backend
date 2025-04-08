ca0372c313bffdae81a12c7914e72ca3fd792316
OscarChaverra <oscarchaverra64@gmail.com>
2025-04-08 13:05:17 -0500

    arreglo de errores en enfermedades

--- enfermedad/views.py
+++ enfermedad/views.py
@@ -3,32 +3,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
-from .serializer import EnfermedadSerializer
-
-from .models import Enfermendad
-
-
-# Create your views here.
-class SaveDiseases(APIView):
-
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        dataInput = request.data
-
-        serializer = EnfermedadSerializer(data=dataInput, many=True)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "diseases saved"}, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
 from .models import Enfermendad
 from .serializer import EnfermedadSerializer
 
@@ -50,7 +24,7 @@ class SaveDiseases(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 class ViewEnfermedades(APIView):
-    permission_classes = [AlalowAny]
+    permission_classes = [AllowAny]
 
     def post(self, request):
         try:
