23d70c44b223082e55c64e9af191883fdca9dfca
OscarChaverra <oscarchaverra64@gmail.com>
2025-04-08 16:20:50 -0500

    cambio en los cors para los permisos

--- Notepads/settings.py
+++ Notepads/settings.py
@@ -199,6 +199,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://sub.example.com",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "https://frontnotepads-production.up.railway.app"
 ]
 
 # settings.py
